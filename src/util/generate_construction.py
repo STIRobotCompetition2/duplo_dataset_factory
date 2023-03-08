ff6f856c9d4c3f628be502e9f6c64711b13e37bb
Sven Becker <sven.becker@epfl.ch>
2023-03-08 21:21:25 +0100

    Fix bugs it works now

--- src/util/generate_construction.py
+++ src/util/generate_construction.py
@@ -61,44 +61,53 @@ def transform(base_mesh: list, new_mesh: list, connection:list, layer:int):
     
 
 def generate_construction(brick_shapes:list, color_palette:list, max_height:int)->trimesh.Trimesh:
-    layers = [[copy.deepcopy(brick_shapes[0])]]
+    layers = [[copy.deepcopy(brick_shapes[3])]]
     uv = np.random.rand(layers[0][0][0].vertices.shape[0], 2)
     next_color = color_palette[np.random.randint(0,len(color_palette))]
     layers[0][0][0].visual = trimesh.visual.TextureVisuals(uv=uv,material=trimesh.visual.material.SimpleMaterial(diffuse=next_color, glossiness=1, ambient=next_color, specular=next_color))
     while len(layers) < max_height + 1:
         new_layer = []
-        # collision_check_grid = np.zeros((100,100))
+        collision_check_grid = np.zeros((100,100))
 
         for brick in layers[-1]:
-            # anchor = (np.array(brick[2][0:2]) / DUPLO_WIDTH).astype(np.int64) + np.ones((2,), dtype=np.int64) * 50
+            anchor = (np.array(brick[2][0:2]) / DUPLO_WIDTH).astype(np.int64) + np.ones((2,), dtype=np.int64) * 50
+            anchor_rot = round(brick[2][2] / (np.pi / 2))
             while np.random.rand() > np.exp(0.2 * len(new_layer)) - 1:
                 next_brick = copy.deepcopy(brick_shapes[np.random.randint(0,len(brick_shapes))])
                 next_color = color_palette[np.random.randint(0,len(color_palette))]
                 # next_brick = copy.deepcopy(BRICK_TYPES[0])
                 uv = np.random.rand(next_brick[0].vertices.shape[0], 2)
                 next_brick[0].visual = trimesh.visual.TextureVisuals(uv=uv,material=trimesh.visual.material.SimpleMaterial(diffuse=next_color, glossiness=1, ambient=None, specular=next_color))
-                next_connection = [np.random.randint(0, brick[1][0]), np.random.randint(0, brick[1][1]), np.random.randint(0,4)]
-                # for _ in range(10):
-                #     corner_A = anchor + np.array(next_connection[0:2])
-                #     corner_B = np.array(next_brick[1])
-                #     for _ in range(next_connection[2]):
-                #         corner_B = np.array([[0,-1],[1,0]], dtype=np.int64) @ corner_B
-                #     corner_B += anchor + np.array(next_connection[0:2])
-                #     min_corner = np.min([corner_B, corner_A],0)
-                #     max_corner = np.max([corner_B, corner_A],0)
-                #     if np.all(collision_check_grid[min_corner[0]:max_corner[0]+1, min_corner[1]:max_corner[1]+1] == 0): break
+                
+                for _ in range(100):
+                    next_connection = [np.random.randint(0, brick[1][0]), np.random.randint(0, brick[1][1]), np.random.randint(0,4)]
+                    corner_A = np.array(next_connection[0:2])
+                    for _ in range(anchor_rot):
+                        corner_A = np.array([[0,-1],[1,0]], dtype=np.int64) @ corner_A
+                    corner_A = corner_A + anchor
+                    corner_B = np.array(next_brick[1]) - 1
+                    for _ in range(next_connection[2]):
+                        corner_B = np.array([[0,-1],[1,0]], dtype=np.int64) @ corner_B
+                    corner_B = corner_B + corner_A
+                    min_corner = np.min([corner_B, corner_A],0)
+                    max_corner = np.max([corner_B, corner_A],0)
+                    if np.all(np.all(collision_check_grid[min_corner[0]:max_corner[0]+1,min_corner[1]:max_corner[1]+1] < 0.1)): break
                     
-                #     # plt.imshow(collision_check_grid)
-                #     # plt.show()
-                # else:
-                #     continue
+                else:
+                    continue
 
-                # collision_check_grid[min_corner[0]:max_corner[0]+1, min_corner[1]:max_corner[1]+1] = 1
+                collision_check_grid[min_corner[0]:max_corner[0]+1, min_corner[1]:max_corner[1]+1] = 1
                 transform(brick, next_brick, next_connection, len(layers))
                 # next_brick[0] = trimesh.Trimesh(next_brick[0].vertices, next_brick[0].faces, visual=trimesh.visual.TextureVisuals(face_materials=trimesh.visual.material.SimpleMaterial(diffuse=next_color, glossiness=1.0, ambient=next_color)))
+            
                 new_layer.append(copy.deepcopy(next_brick))
+
+
+                # plt.imshow(collision_check_grid)
+                # plt.show()
+
         layers.append(new_layer)
-        
+
     return layers
 
 if __name__ == "__main__":
